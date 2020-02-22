ba02209f54a247cb94b96dde79dd45ed1ebde55b
ChenTing Yu <yu18002@byui.edu>
2020-02-22 13:04:52 -0700

    File handler is working!! The test webpage is working too now!

--- src/main/java/handler/FileHandler.java
+++ src/main/java/handler/FileHandler.java
@@ -21,24 +21,26 @@ public class FileHandler implements HttpHandler {
         if(exchange.getRequestMethod().toUpperCase().equals("GET")) {
             String urlPath = exchange.getRequestURI().toString();
 
-            if(urlPath == null || urlPath == "/" || urlPath.equals("")) {
+            if(urlPath == null || urlPath.equals("/") || urlPath.equals("")) {
                 urlPath = "/index.html";
             }
-//            String workingDirectory = System.getProperty("user.dir");
-//            System.out.println(workingDirectory);
-            String workingDirectory = "web" + urlPath;
 
-            if(!new File(workingDirectory).exists()) {
+            String workingDirectory = "web" + urlPath;
+            File file = new File(workingDirectory);
+            System.out.println(workingDirectory);
+
+            if(!file.exists()) {
                 workingDirectory = System.getProperty("user.dir") + "/web/HTML/404.html";
+                file = new File(System.getProperty("user.dir") + "/web/HTML/404.html");
                 exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+            } else {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
             }
 
             Path filePath = FileSystems.getDefault().getPath(workingDirectory);
-
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
             OutputStream respBody = exchange.getResponseBody();
             Files.copy(filePath, respBody);
-            exchange.getRequestBody().close();
+            respBody.close();
 
 
 
